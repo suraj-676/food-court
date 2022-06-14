b3c1fd85f6d2ba9cecb235ba904556b29736ef3a
suraj sharma <surajneha2510@gmail.com@yourdomain.com>
2022-06-14 12:42:27 +1000

    working on javascript

--- js/main.js
+++ js/main.js
@@ -1,1 +1,17 @@
-console.log('Hello, World')
+ console.log('Hello, World')
+let menu_item = document.getElementById("menu_item")
+let bars = document.getElementById("bars")
+
+bars.addEventListener("click", () => {
+    if(menu_item.style.opacity=="0") {
+        menu_item.style.opacity="1";
+        menu_item.style.height=="100vh";
+    } else {
+        menu_item.style.opacity=="0";
+        menu_item.style.height=="0vh";
+
+
+    }
+
+
+})
