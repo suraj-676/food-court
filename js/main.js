c277d542a3bd40b543de141a33ee245ec22b8ca4
suraj sharma <surajneha2510@gmail.com@yourdomain.com>
2022-06-15 12:21:51 +1000

    written javascript

--- js/main.js
+++ js/main.js
@@ -1,14 +1,14 @@
  console.log('Hello, World')
-let menu_item = document.getElementById("menu_item")
+let menu_tem = document.getElementById("menu_item")
 let bars = document.getElementById("bars")
 
 bars.addEventListener("click", () => {
-    if(menu_item.style.opacity=="0") {
-        menu_item.style.opacity="1";
-        menu_item.style.height=="100vh";
+    if(menu_item.style.opacity == "0") {
+        menu_item.style.opacity = "1";
+        menu_item.style.height = "70vh";
     } else {
-        menu_item.style.opacity=="0";
-        menu_item.style.height=="0vh";
+        menu_item.style.opacity = "0";
+        menu_item.style.height = "0vh";
 
 
     }
